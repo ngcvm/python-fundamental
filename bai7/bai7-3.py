99b189f9b90c969c03c5761ea27dd12fffd4403f
me <me@me_again.com>
2020-09-06 19:45:37 +0800

    Excercise 7.3 - 7.4

--- bai7/bai7-3.py
+++ bai7/bai7-3.py
@@ -2,5 +2,38 @@
 # Bai 7.3
 @author: packkkk
 '''
-#Code goes here!!!
+import math
 
+def is_prime_number (num):
+    if (num < 2):
+        return False
+    else:
+        counter = 2
+        counter_guard = 0
+        while counter <= math.sqrt(num):
+            if num % counter == 0:
+                counter_guard += 1
+            counter += 1
+        return True if counter_guard == 0 else False
+
+arth_mean_cal = lambda l: sum(l) / len(l)
+
+the_list = []
+break_input = 1
+while break_input == 1:
+    the_list.append(int(input('Input value: ')))
+    break_input = int(input('Do you want to add more value? 0 (or anykey): No and 1: Yes'))
+
+negative_nums = [num for num in the_list if num < 0]
+positive_nums = [num for num in the_list if num > 0]
+
+print('List: ', list(the_list))
+print('Prime numbers in list: ', [i for i in the_list if is_prime_number(i)])
+print('Negative numbers in list: ', negative_nums)
+print('Positive numbers in list: ', positive_nums)
+print('Arithmetic mean of all negative and positive number in the list is: %.2f, %.2f'%(arth_mean_cal(negative_nums), arth_mean_cal(positive_nums)))
+print('Max number: %i, Min number: %i'%(max(the_list), min(the_list)))
+temp_list = the_list[:] #or the_list.copy() or copy.copy(the_list) or copy.deepcopy(the_list)
+the_list.sort()
+print('Sort asc with list.sort(): ', the_list)
+print('Sort asc with sorted(): ', sorted(temp_list))
--- bai7/bai7-4.py
+++ bai7/bai7-4.py
@@ -3,5 +3,20 @@ Bai 7.4
 @author: packkkk
 '''
 
-#Code goes here!!!
+the_tuple = tuple(input('Input tuple (separate by ","): ').split(','))
+the_tuple = tuple([i.strip() for i in the_tuple])
+print('Tuple: ', the_tuple)
+
+positive_index = int(input('Input positive index   (from 0 to 9): '))
+negative_index = int(input('Input negative index   (from -1 to -9): '))
+if positive_index < 0 or positive_index > 9:
+    raise Exception('Positive index cannot exceed the range 0 - 9')
+if negative_index > -1 or negative_index <-9:
+    raise Exception('Negative index cannot exceed the range -9 - -1')
+
+s_find = input('Input string to find: ')
+print(type(the_tuple))
+print ('Tuple[%i] = %s'%(positive_index, the_tuple[positive_index]))
+print ('Tuple[%i] = %s'%(negative_index, the_tuple[negative_index]))
+print ('%s appears %i times in the tuple'%(s_find, the_tuple.count(s_find)))
 
